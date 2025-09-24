b3a7de0fcea90fadec06b872949c9fd6a646b4b3
phantom-mayavi <phantom.mayavi@gmail.com>
2025-09-24 17:35:45 -0400

    init: Spring Boot project scaffold

---
+++ src/main/java/com/example/spring_boot_llm_text_summarizer/SpringBootLlmTextSummarizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_llm_text_summarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLlmTextSummarizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLlmTextSummarizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_llm_text_summarizer/SpringBootLlmTextSummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_llm_text_summarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLlmTextSummarizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
